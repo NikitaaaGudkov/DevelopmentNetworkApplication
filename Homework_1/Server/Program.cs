3f4f20df28ec7986b14f0c5f787ce9502e284b31
NikitaaaGudkov <nikita.gudkov.2015@mail.ru>
2024-02-19 19:29:28 +0300

    Добавьте файлы проекта.

---
+++ Homework_1/Server/Program.cs
@@ -0,0 +1,87 @@
+﻿// Попробуйте переработать приложение, добавив подтверждение об отправке сообщений как в сервер, так и в клиент.
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpChat
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new ChatServer();
+
+            await server.Run();
+        }
+    }
+
+    public class ChatServer
+    {
+        List<User> users = new List<User>();
+
+        TcpListener listener = new TcpListener(IPAddress.Any, 55555);
+        public async Task Run()
+        {
+            try
+            {
+                listener.Start();
+
+                await Console.Out.WriteLineAsync("Запущен");
+
+                while (true)
+                {
+                    var tcpClient = await listener.AcceptTcpClientAsync();
+
+                    var user = new User(tcpClient, this);
+
+                    users.Add(user);
+
+                    _ = Task.Run(user.ProcessAsync);
+                }
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        public async Task BroadcastMessageAsync(string message, string id)
+        {
+            foreach (var user in users)
+            {
+                if (user.Id != id)
+                {
+                    await user.Writer.WriteLineAsync(message);
+
+                    await user.Writer.FlushAsync();
+                }
+            }
+        }
+
+        public void RemoveConnection(string id)
+        {
+            User? user = users.FirstOrDefault(u => u.Id == id);
+
+            if (user != null)
+            {
+                users.Remove(user);
+
+                user.Close();
+            }
+        }
+
+        public void Disconnect()
+        {
+            foreach (var user in users)
+            {
+                user.Close();
+            }
+
+            listener.Stop();
+        }
+    }
+}
---
+++ Homework_1/Server/User.cs
@@ -0,0 +1,89 @@
+﻿using System.Net.Sockets;
+
+namespace TcpChat
+{
+    internal class User
+    {
+        public string Id { get; }
+        public StreamWriter Writer { get; }
+        public StreamReader Reader { get; }
+
+        private TcpClient client;
+
+        private ChatServer server;
+
+        public User(TcpClient client, ChatServer server)
+        {
+            Id = Guid.NewGuid().ToString();
+
+            this.client = client;
+
+            this.server = server;
+
+            var stream = client.GetStream();
+
+            Reader = new StreamReader(stream);
+
+            Writer = new StreamWriter(stream);
+        }
+
+        public async Task ProcessAsync()
+        {
+            try
+            {
+                string? userName = await Reader.ReadLineAsync();
+
+                string? message = $"{userName} вошёл в чат";
+
+                await server.BroadcastMessageAsync(message, Id);
+
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    try
+                    {
+                        message = await Reader.ReadLineAsync();
+
+                        if (message == null)
+                        {
+                            continue;
+                        }
+                        message = $"{userName}: {message}";
+
+                        Console.WriteLine(message);
+
+                        await server.BroadcastMessageAsync(message, Id);
+                    }
+                    catch
+                    {
+                        message = $"{userName} покинул чат";
+
+                        Console.WriteLine(message);
+
+                        await server.BroadcastMessageAsync(message, Id);
+
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                server.RemoveConnection(Id);
+            }
+        }
+
+        public void Close()
+        {
+            Writer.Close();
+
+            Reader.Close();
+
+            client.Close();
+        }
+    }
+}
