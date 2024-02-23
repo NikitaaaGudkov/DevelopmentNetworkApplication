89db7df1a3eb0d8d66125c47c6034d5313171c67
NikitaaaGudkov <nikita.gudkov.2015@mail.ru>
2024-02-23 17:37:57 +0300

    Homework_2 done

---
+++ Homework_2/Client/Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Net.Sockets;
+
+namespace TcpChat
+{
+    internal class Program
+    {
+        static string name = string.Empty;
+        static async Task Main(string[] args)
+        {
+            var tcpClient = new TcpClient();
+
+            while (true)
+            {
+                Console.Write("Введите своё имя: ");
+
+                name = Console.ReadLine()!;
+
+                if (name is null || name.Length == 0)
+                {
+                    Console.WriteLine("Вы не ввели ваше имя!");
+                }
+                else
+                {
+                    Console.WriteLine($"Добро пожаловать, {name}");
+                    break;
+                }
+            }
+
+            StreamReader? reader = null;
+
+            StreamWriter? writer = null;
+
+            try
+            {
+                tcpClient.Connect("localhost", 55555);
+
+                reader = new StreamReader(tcpClient.GetStream());
+
+                writer = new StreamWriter(tcpClient.GetStream());
+
+                if (reader is null || writer is null)
+                {
+                    Console.WriteLine("Не удалось подключиться к серверу. Для выхода из программы нажмите любую клавишу");
+
+                    Console.ReadLine();
+
+                    return;
+                }
+
+                _ = Task.Run(() => ReceiveMessageAsync(reader));
+
+                await SendMessageAsync(writer);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer?.Close();
+
+                reader?.Close();
+            }
+
+            async Task SendMessageAsync(StreamWriter writer)
+            {
+                await writer.WriteLineAsync(name);
+
+                await writer.FlushAsync();
+
+                Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+
+                while (true)
+                {
+                    string? message = Console.ReadLine();
+
+                    await writer.WriteLineAsync(message);
+
+                    await writer.FlushAsync();
+
+                    if(message == "Exit")
+                    {
+                        return;
+                    }
+                }
+            }
+
+            async Task ReceiveMessageAsync(StreamReader reader)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        string? message = await reader.ReadLineAsync();
+
+                        if (string.IsNullOrEmpty(message))
+                        {
+                            continue;
+                        }
+
+                        Print(message);
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+            }
+
+            // Метод, переносящий набираемое сообщение, если в процессе написания были получены новые сообщения от сервера.
+            void Print(string message)
+            {
+                if (OperatingSystem.IsWindows())
+                {
+                    var position = Console.GetCursorPosition();
+
+                    int left = position.Left;
+
+                    int top = position.Top;
+
+                    Console.MoveBufferArea(0, top, left, 1, 0, top + 1);
+
+                    Console.SetCursorPosition(0, top);
+
+                    Console.WriteLine(message);
+
+                    Console.SetCursorPosition(left, top + 1);
+                }
+                else
+                {
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
---
+++ Homework_2/Server/Program.cs
@@ -0,0 +1,106 @@
+﻿//Добавьте возможность ввести слово Exit в чате клиента, чтобы можно было завершить его работу.
+//В коде сервера добавьте ожидание нажатия клавиши, чтобы также прекратить его работу.
+
+//** Добавляем многопоточность в чат позволяя серверной части получать сообщения сразу от нескольких респондентов.
+//Временно удалим из сервера возможность ввода сообщений. Сделаем так чтобы чат всегда отвечал “Сообщение получено”.
+//Протестируем наш чат запустив сразу 10 клиентов. Для удобства сделаем так чтобы клиент также ничего не запускал
+//но просто слал привет.
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpChat
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new ChatServer();
+
+            await server.Run();
+        }
+    }
+
+    public class ChatServer
+    {
+        List<User> users = new List<User>();
+
+        TcpListener listener = new TcpListener(IPAddress.Any, 55555);
+        public async Task Run()
+        {
+            try
+            {
+                var thread = new Thread(FinishServer);
+
+                thread.Start();
+
+                listener.Start();
+
+                await Console.Out.WriteLineAsync("Запущен");
+
+                while (true)
+                {
+                    var tcpClient = await listener.AcceptTcpClientAsync();
+
+                    var user = new User(tcpClient, this);
+
+                    users.Add(user);
+
+                    _ = Task.Run(user.ProcessAsync);
+                }
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        public async Task BroadcastMessageAsync(string message, string id)
+        {
+            foreach (var user in users)
+            {
+                if (user.Id != id)
+                {
+                    await user.Writer.WriteLineAsync(message);
+
+                    await user.Writer.FlushAsync();
+                }
+            }
+        }
+
+        public void RemoveConnection(string id)
+        {
+            User? user = users.FirstOrDefault(u => u.Id == id);
+
+            if (user != null)
+            {
+                users.Remove(user);
+
+                user.Close();
+            }
+        }
+
+        public void Disconnect()
+        {
+            foreach (var user in users)
+            {
+                user.Close();
+            }
+
+            listener.Stop();
+        }
+
+        public void FinishServer()
+        {
+            string? str = Console.ReadLine();
+            if(str != null)
+            {
+                Disconnect();
+            }
+        }
+    }
+}
